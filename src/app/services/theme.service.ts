a3920566d2d2ffc81595946270465ba5974bdcf8
i. dos santos <iosorio.santos@gmail.com>
2025-01-07 16:14:46 -0300

    implement theme-service

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeService {
+  private themeKey = 'theme';
 
-  constructor() { }
+  constructor() {}
+
+  applyTheme(theme: 'dark' | 'light'): void {
+    document.documentElement.setAttribute('data-p-color-scheme', theme);
+    localStorage.setItem(this.themeKey, theme);
+  }
+
+  getTheme(): 'dark' | 'light' {
+    return (localStorage.getItem(this.themeKey) as 'dark' | 'light') || 'dark';
+  }
+
+  toggleTheme(): void {
+    const currentTheme = this.getTheme();
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    this.applyTheme(newTheme);
+  }
 }
