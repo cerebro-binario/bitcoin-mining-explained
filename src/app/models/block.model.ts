572aebf0b70272a6e242ea9cba167d6ed23941f9
i. dos santos <iosorio.santos@gmail.com>
2025-01-13 13:30:58 -0300

    add model for Block

---
+++ src/app/models/block.model.ts
@@ -0,0 +1,11 @@
+import { Transaction } from './transaction.model';
+
+export interface Block {
+  previousHash: string;
+  height: number;
+  transactions: Transaction[];
+  timestamp: string;
+  merkleRoot?: string;
+  nonce?: number;
+  hash?: string;
+}
