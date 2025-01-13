d6dbcb16677b0b19d5182de0e7230979c8888eb5
i. dos santos <iosorio.santos@gmail.com>
2025-01-13 14:52:39 -0300

    create global utils functions

---
+++ src/app/utils/tools.ts
@@ -0,0 +1,27 @@
+import { createHash } from 'crypto';
+
+export function hashSHA256(data: string): string {
+  return createHash('sha256').update(data).digest('hex');
+}
+
+export function getRandomAmount(max = 10): number {
+  return parseFloat((Math.random() * max).toFixed(8));
+}
+
+export function getRandomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function getWeightedRandomInput(): number {
+  const weights = [70, 20, 10];
+  const cumulativeWeights = weights.map(
+    (
+      (sum) => (value) =>
+        (sum += value)
+    )(0)
+  );
+  const random =
+    Math.random() * cumulativeWeights[cumulativeWeights.length - 1];
+
+  return cumulativeWeights.findIndex((weight) => random < weight) + 1;
+}
