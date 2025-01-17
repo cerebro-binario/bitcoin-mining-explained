f2457853cf0a10e7c47f254d03a76436fd799ff6
i. dos santos <iosorio.santos@gmail.com>
2025-01-16 22:40:39 -0300

    hardcode the genesis block

--- src/app/models/block.model.ts
+++ src/app/models/block.model.ts
@@ -1,3 +1,4 @@
+import { GENESIS_KEY_PAIR } from './address.model';
 import { Transaction } from './transaction.model';
 
 export interface Block {
@@ -9,3 +10,27 @@ export interface Block {
   nonce: number;
   hash: string;
 }
+
+export const GENESIS_BLOCK: Block = {
+  previousHash:
+    '0000000000000000000000000000000000000000000000000000000000000000',
+  height: 0,
+  transactions: [
+    {
+      txid: '54e39cc08806e3809f279abac25867ae3206c5892eb1df69ac51b7c5dbe11be0',
+      fee: 0,
+      transfers: [
+        {
+          amount: 50.0,
+          to: GENESIS_KEY_PAIR.addresses['P2PKH'].address,
+          from: '0000000000000000000000000000000000000000000000000000000000000000',
+        },
+      ],
+    },
+  ],
+  timestamp: 1737069357,
+  hash: '0000d590a982a9489b64e91cea5e8f81635c56718b1c459041bd3a3a189cf6aa',
+  merkleRoot:
+    '54e39cc08806e3809f279abac25867ae3206c5892eb1df69ac51b7c5dbe11be0',
+  nonce: 346257,
+};
