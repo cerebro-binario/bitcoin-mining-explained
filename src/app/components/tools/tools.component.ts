515a0c914370d614962a2ed3f9cca3e4d157aa9a
i. dos santos <iosorio.santos@gmail.com>
2025-04-25 08:19:24 -0300

    feat: add fullscreen mode to tools sections with performance optimizations

--- src/app/components/tools/tools.component.ts
+++ src/app/components/tools/tools.component.ts
@@ -5,10 +5,15 @@ import { HashesComparedComponent } from './hashes-compared/hashes-compared.compo
 import { HexadecimalComponent } from './hexadecimal/hexadecimal.component';
 import { DiceAnalogyComponent } from './dice-analogy/dice-analogy.component';
 import { BitcoinEmissionChartComponent } from './bitcoin-emission-chart/bitcoin-emission-chart.component';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-tools',
+  templateUrl: './tools.component.html',
+  styleUrls: ['./tools.component.scss'],
+  standalone: true,
   imports: [
+    TooltipModule,
     HexadecimalComponent,
     HashComponent,
     HashesComparedComponent,
@@ -16,7 +21,26 @@ import { BitcoinEmissionChartComponent } from './bitcoin-emission-chart/bitcoin-
     DiceAnalogyComponent,
     BitcoinEmissionChartComponent,
   ],
-  templateUrl: './tools.component.html',
-  styleUrl: './tools.component.scss',
 })
-export class ToolsComponent {}
+export class ToolsComponent {
+  isFullscreen: { [key: string]: boolean } = {
+    hexadecimal: false,
+    hash: false,
+    'hashes-compared': false,
+    'dice-analogy': false,
+    'hash-target': false,
+    'bitcoin-emission-chart': false,
+  };
+
+  toggleFullscreen(section: string) {
+    // First, minimize all other sections
+    Object.keys(this.isFullscreen).forEach((key) => {
+      if (key !== section) {
+        this.isFullscreen[key] = false;
+      }
+    });
+
+    // Then toggle the selected section
+    this.isFullscreen[section] = !this.isFullscreen[section];
+  }
+}
