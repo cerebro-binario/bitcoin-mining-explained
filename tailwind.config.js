6c3e8824f95f41a31d410b48eca9d8b07cfe87cc
i. dos santos <iosorio.santos@gmail.com>
2025-05-02 12:21:08 -0300

    fix: tailwind config to enable autocomplete/intelisense

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,28 @@
-import plugin from "tailwindcss/plugin";
+const plugin = require("tailwindcss/plugin");
 
 // tailwind.config.js
-export const plugins = [
-  require("tailwindcss-primeui"),
-  plugin(function ({ addUtilities }) {
-    addUtilities({
-      ".text-accent": { color: "var(--p-accent-text-color)" },
-      ".text-accent-hover:hover": { color: "var(--p-accent-hover-color)" },
-      ".text-accent-active:active": { color: "var(--p-accent-active-color)" },
-    });
-  }),
-];
-export const content = ["./public/index.html", "./src/**/*.{html,js,ts,scss}"];
-export const theme = {
-  extend: {
-    colors: {
-      accent: {
-        DEFAULT: "var(--p-accent-text-color)",
-        hover: "var(--p-accent-hover-color)",
-        active: "var(--p-accent-active-color)",
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js,ts,scss}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          DEFAULT: "var(--p-accent-text-color)",
+          hover: "var(--p-accent-hover-color)",
+          active: "var(--p-accent-active-color)",
+        },
       },
     },
   },
+  plugins: [
+    require("tailwindcss-primeui"),
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".text-accent": { color: "var(--p-accent-text-color)" },
+        ".text-accent-hover:hover": { color: "var(--p-accent-hover-color)" },
+        ".text-accent-active:active": { color: "var(--p-accent-active-color)" },
+      });
+    }),
+  ],
 };
-export const darkMode = "class";
