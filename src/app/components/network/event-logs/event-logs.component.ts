b998b22f43a7ce32b562762722ade4b38a76e02f
i. dos santos <iosorio.santos@gmail.com>
2025-05-05 14:00:53 -0300

    refac: isolate event logs in its own component

---
+++ src/app/components/network/event-logs/event-logs.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Node } from '../../../models/node';
+
+@Component({
+  selector: 'app-event-logs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './event-logs.component.html',
+  styleUrls: ['./event-logs.component.scss'],
+})
+export class EventLogsComponent {
+  @Input() node!: Node;
+  @Output() show = new EventEmitter<void>();
+  @Output() close = new EventEmitter<void>();
+
+  showAllLogs = false;
+
+  constructor() {}
+
+  showNodeLogs() {
+    this.showAllLogs = true;
+    this.show.emit();
+  }
+
+  closeLogs() {
+    this.showAllLogs = false;
+    this.close.emit();
+  }
+}
