8092a41ee1b8c82107d352e49bb7a8265dca0e54
i. dos santos <iosorio.santos@gmail.com>
2025-05-12 11:31:42 -0300

    patch: add backdrop effect

--- src/app/components/network/miners-panel/miner/peers-dialog/peers-dialog.component.ts
+++ src/app/components/network/miners-panel/miner/peers-dialog/peers-dialog.component.ts
@@ -1,6 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnInit,
+  OnDestroy,
+  Inject,
+  ElementRef,
+} from '@angular/core';
 import { Node } from '../../../../../models/node';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-peers-dialog',
@@ -9,11 +19,29 @@ import { Node } from '../../../../../models/node';
   templateUrl: './peers-dialog.component.html',
   styleUrls: ['./peers-dialog.component.scss'],
 })
-export class PeersDialogComponent {
+export class PeersDialogComponent implements OnInit, OnDestroy {
   @Input() miner!: Node;
   @Output() close = new EventEmitter<void>();
 
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private elRef: ElementRef
+  ) {}
+
+  ngOnInit() {
+    this.document.body.classList.add('overflow-hidden');
+  }
+  ngOnDestroy() {
+    this.document.body.classList.remove('overflow-hidden');
+  }
+
   onClose() {
     this.close.emit();
+  }
+
+  onBackdropClick(event: MouseEvent) {
+    if (event.target === event.currentTarget) {
+      this.onClose();
+    }
   }
 }
