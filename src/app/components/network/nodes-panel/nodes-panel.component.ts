1e4d236fc5e4a4ad5bb439a4f463232901e3bf6f
i. dos santos <iosorio.santos@gmail.com>
2025-05-28 21:48:41 -0300

    patch: make nodes header similar to miners header

--- src/app/components/network/nodes-panel/nodes-panel.component.ts
+++ src/app/components/network/nodes-panel/nodes-panel.component.ts
@@ -5,16 +5,25 @@ import { FormsModule } from '@angular/forms';
 import { Node } from '../../../models/node';
 import { BitcoinNetworkService } from '../../../services/bitcoin-network.service';
 import { EventsComponent } from '../events/events.component';
+import { ConsensusDialogComponent } from '../miners-panel/miner/consensus-dialog/consensus-dialog.component';
 
 @Component({
   selector: 'app-nodes-panel',
   standalone: true,
-  imports: [CommonModule, FormsModule, EventsComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    EventsComponent,
+    ConsensusDialogComponent,
+  ],
   templateUrl: './nodes-panel.component.html',
   styleUrls: ['./nodes-panel.component.scss'],
 })
 export class NodesPanelComponent {
   nodes: Node[] = [];
+  showConsensusDialog = false;
+  consensusNode: Node | null = null;
+
   constructor(public network: BitcoinNetworkService) {
     this.network.nodes$.pipe(takeUntilDestroyed()).subscribe((nodes) => {
       this.nodes = nodes.filter((n) => !n.isMiner);
@@ -63,5 +72,15 @@ export class NodesPanelComponent {
 
   searchPeers(node: Node) {
     node.searchPeersToConnect(this.network.nodes);
+  }
+
+  editConsensusParams(node: Node) {
+    this.consensusNode = node;
+    this.showConsensusDialog = true;
+  }
+
+  closeConsensusDialog() {
+    this.showConsensusDialog = false;
+    this.consensusNode = null;
   }
 }
