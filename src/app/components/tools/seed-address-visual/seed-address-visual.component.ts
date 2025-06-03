c7ba31979d18ce0fa82742b9138dcc07184e1709
i. dos santos <iosorio.santos@gmail.com>
2025-06-03 15:53:18 -0300

    improve control

--- src/app/components/tools/seed-address-visual/seed-address-visual.component.ts
+++ src/app/components/tools/seed-address-visual/seed-address-visual.component.ts
@@ -168,6 +168,8 @@ export class SeedAddressVisualComponent {
   pubKey: string = '';
   addresses: string[] = [];
   saldos: number[] = [];
+  isAutoRunning: boolean = false;
+  autoTimeout: any = null;
 
   startBruteForceManual() {
     this.animationStep = 1;
@@ -179,24 +181,22 @@ export class SeedAddressVisualComponent {
   }
 
   nextStepManual() {
+    this.pauseAuto();
     if (this.animationStep === 0) {
       this.startBruteForceManual();
       return;
     }
     if (this.animationStep === 1) {
-      // Derivar chave priv/pub
       this.privKey = 'priv_' + Math.random().toString(36).slice(2, 10);
       this.pubKey = 'pub_' + Math.random().toString(36).slice(2, 10);
     }
     if (this.animationStep === 2) {
-      // Derivar vários endereços
       this.addresses = [];
       for (let i = 0; i < 5; i++) {
         this.addresses.push('bc1q' + Math.random().toString(36).slice(2, 12));
       }
     }
     if (this.animationStep === 3) {
-      // Consultar saldo de todos os endereços
       this.saldos = this.addresses.map(() => 0);
     }
     if (this.animationStep < 5) {
@@ -205,17 +205,73 @@ export class SeedAddressVisualComponent {
   }
 
   prevStepManual() {
+    this.pauseAuto();
     if (this.animationStep > 1) {
       this.animationStep--;
     }
   }
 
   resetManual() {
+    this.pauseAuto();
     this.animationStep = 0;
     this.seedWords = [];
     this.privKey = '';
     this.pubKey = '';
     this.addresses = [];
     this.saldos = [];
+  }
+
+  toggleAutoRun() {
+    if (this.isAutoRunning) {
+      this.pauseAuto();
+    } else {
+      if (this.animationStep === 0 || this.animationStep === 5) {
+        this.startBruteForceManual();
+        this.animationStep = 1;
+      }
+      this.isAutoRunning = true;
+      this.autoNext();
+    }
+  }
+
+  autoNext() {
+    if (!this.isAutoRunning) return;
+    if (this.animationStep < 5) {
+      this.nextStepAuto();
+      this.autoTimeout = setTimeout(() => this.autoNext(), 1200);
+    } else {
+      this.isAutoRunning = false;
+    }
+  }
+
+  nextStepAuto() {
+    if (this.animationStep === 0) {
+      this.startBruteForceManual();
+      return;
+    }
+    if (this.animationStep === 1) {
+      this.privKey = 'priv_' + Math.random().toString(36).slice(2, 10);
+      this.pubKey = 'pub_' + Math.random().toString(36).slice(2, 10);
+    }
+    if (this.animationStep === 2) {
+      this.addresses = [];
+      for (let i = 0; i < 5; i++) {
+        this.addresses.push('bc1q' + Math.random().toString(36).slice(2, 12));
+      }
+    }
+    if (this.animationStep === 3) {
+      this.saldos = this.addresses.map(() => 0);
+    }
+    if (this.animationStep < 5) {
+      this.animationStep++;
+    }
+  }
+
+  pauseAuto() {
+    this.isAutoRunning = false;
+    if (this.autoTimeout) {
+      clearTimeout(this.autoTimeout);
+      this.autoTimeout = null;
+    }
   }
 }
