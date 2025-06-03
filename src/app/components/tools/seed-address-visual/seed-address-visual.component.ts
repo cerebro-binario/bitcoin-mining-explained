b45cd00be81e8df32b444b0050d7a206b6ea15b2
i. dos santos <iosorio.santos@gmail.com>
2025-06-03 15:26:08 -0300

    patch: add visual explanation about seed/address generation

---
+++ src/app/components/tools/seed-address-visual/seed-address-visual.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-seed-address-visual',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './seed-address-visual.component.html',
+  styleUrls: ['./seed-address-visual.component.scss'],
+})
+export class SeedAddressVisualComponent {}
