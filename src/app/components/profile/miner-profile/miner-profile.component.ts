c00a5979840bb3215e9e2cacff6c878385ab57f1
i. dos santos <iosorio.santos@gmail.com>
2025-06-17 07:58:42 -0300

    patch: init UI for miner

--- src/app/components/profile/miner-profile/miner-profile.component.ts
+++ src/app/components/profile/miner-profile/miner-profile.component.ts
@@ -1,8 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import type { Node as BitcoinNode } from '../../../models/node';
+import { BitcoinNetworkService } from '../../../services/bitcoin-network.service';
 
 @Component({
   selector: 'app-miner-profile',
   templateUrl: './miner-profile.component.html',
   styleUrls: ['./miner-profile.component.scss'],
+  imports: [CommonModule, RouterModule],
 })
-export class MinerProfileComponent {}
+export class MinerProfileComponent {
+  miner: BitcoinNode | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    public bitcoinNetwork: BitcoinNetworkService
+  ) {
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      this.miner = this.bitcoinNetwork.nodes.find(
+        (n) => n.id === id && n.nodeType === 'miner'
+      );
+    });
+  }
+}
