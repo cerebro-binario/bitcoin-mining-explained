a43a3d85f2af6ef01d217cd44afb4d8185d9dd4d
i. dos santos <iosorio.santos@gmail.com>
2025-06-17 07:42:58 -0300

    fix: content injection in route

--- src/app/layouts/profile-page.layout.ts
+++ src/app/layouts/profile-page.layout.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-profile-page-layout',
   templateUrl: './profile-page.layout.html',
   styleUrls: ['./profile-page.layout.scss'],
+  imports: [RouterModule],
 })
 export class ProfilePageLayout {
   back() {
