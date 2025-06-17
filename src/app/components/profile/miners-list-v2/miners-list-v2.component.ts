cf8e7544cfa311489f425e7b84851739a09193ea
i. dos santos <iosorio.santos@gmail.com>
2025-06-17 07:12:48 -0300

    patch: integrate with service

--- src/app/components/profile/miners-list-v2/miners-list-v2.component.ts
+++ src/app/components/profile/miners-list-v2/miners-list-v2.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { BitcoinNetworkService } from '../../../services/bitcoin-network.service';
 
 @Component({
   selector: 'app-miners-list-v2',
   templateUrl: './miners-list-v2.component.html',
   styleUrls: ['./miners-list-v2.component.scss'],
 })
-export class MinersListV2Component {}
+export class MinersListV2Component {
+  constructor(public bitcoinNetwork: BitcoinNetworkService) {}
+
+  addMiner() {
+    this.bitcoinNetwork.addNode('miner');
+  }
+}
