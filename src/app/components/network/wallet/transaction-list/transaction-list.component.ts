742b2eec84c5b80b0236013a2bdbb3a7c7613f45
i. dos santos <iosorio.santos@gmail.com>
2025-06-27 10:56:04 -0300

    patch: make details more visual

--- src/app/components/network/wallet/transaction-list/transaction-list.component.ts
+++ src/app/components/network/wallet/transaction-list/transaction-list.component.ts
@@ -4,6 +4,20 @@ import { RouterLink } from '@angular/router';
 import { TableModule } from 'primeng/table';
 import { TransactionView } from '../wallet.component';
 
+// Função utilitária para copiar para a área de transferência
+function copyToClipboard(text: string) {
+  if (navigator && navigator.clipboard) {
+    navigator.clipboard.writeText(text);
+  } else {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+  }
+}
+
 @Component({
   selector: 'app-transaction-list',
   standalone: true,
@@ -17,4 +31,8 @@ export class TransactionListComponent {
   @Input() currentPage: number = 1;
   @Input() pageSize: number = 10;
   @Input() nodeId: number | null = null;
+
+  copyToClipboard(text: string) {
+    copyToClipboard(text);
+  }
 }
